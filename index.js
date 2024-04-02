b5909b3e5ba45f2b3a06331c341db2865a04aed8
Anthony Santos <anthony.santos1824@gmail.com>
2024-04-02 17:33:06 -0400

    debugged and now fucntional

--- index.js
+++ index.js
@@ -1,66 +1,98 @@
-
 // TODO: Include packages needed for this application
 const inquirer = require("inquirer");
 const fs = require("fs");
 const { generate } = require("rxjs");
 // TODO: Create an array of questions for user input
-const questions = [
-    inquirer
-    .prompt([
-      {
-        type: "input",
-        message: "What is your Project title?",
-        name: "projectTitle",
-      },
-      {
-        type: "input",
-        message: "Type your project description",
-        name: "description",
-      },
-      {
-        type: "input",
-        message: "What are the steps for installing your project?",
-        name: "installInstruction",
-      },
-      {
-        type: "input",
-        message: "Type the usage information",
-        name: "information",
-      },
-      {
-        type: "input",
-        message: "Type the contribution guidelines",
-        name: "guidlines",
-      },
-      {
-        type: "input",
-        message: "Type the test instructions",
-        name: "testInstuction",
-      },
-    ])
-    
-    
-];
-const readMeContent = generateReadme(answers)
-// TODO: Create a function to write README file
-function writeToFile(fileName, data) {
-    fs.writeFile("README.md", readMeContent, (err) => {
-        if (err) throw err;
-        console.log('The file has been saved!');
-    });
-}
 
-const generateReadme =({ projectTitle, description, installInstruction, information, guidelines, testInstruction}) =>
-`# Project Title:  ${projectTitle} 
+  inquirer.prompt([
+    {
+      type: "input",
+      message: "What is your Project title?",
+      name: "projectTitle",
+    },
+    {
+      type: "input",
+      message: "Type your project description",
+      name: "description",
+    },
+    {
+      type: "input",
+      message: "What are the steps for installing your project?",
+      name: "installInstruction",
+    },
+    {
+      type: "input",
+      message: "Type the usage information",
+      name: "usage",
+    },
+    {
+      type: "input",
+      message: "Type the contribution guidelines",
+      name: "guidlines",
+    },
+    {
+      type: "input",
+      message: "Type the test instructions",
+      name: "testInstuction",
+    },
+    {
+      type: "input",
+      message: "Type the License",
+      name: "licenseText",
+    }
+    
+])
+.then((answers) => {
+  const readMeContent = generateReadme(answers);
+  // TODO: Create a function to write READ
+
+  fs.writeFile("README.md", readMeContent, (err) => {
+    if (err) throw err;
+    console.log("The file has been saved!");
+  });
+});
+
+const generateReadme = ({
+  projectTitle,
+  description,
+  installInstruction,
+  usage,
+  guidelines,
+  testInstruction,
+  licenseText
+}) =>
+  `# Project Title:  ${projectTitle} 
 
 ## Description 
 ${description}
-`
 
-// TODO: Create a function to initialize app
-function init() {}
+## Table of content
+### Installation Instructions
+### Usage 
+### Contribution 
+### Test instruction
+### license 
 
-// Function call to initialize app
-init();
 
-description, installation instructions, usage information, contribution guidelines, and test instructions
+
+## Installation instructions
+${installInstruction}
+
+## Usage information
+${usage}
+
+## Contribution Guidlines 
+${guidelines}
+
+## Test Instruction 
+${testInstruction}
+
+## License
+${licenseText}
+
+`;
+
+// // TODO: Create a function to initialize app
+
+// // Function call to initialize app
+
